0f9657652c67f2031ea8a40f167008cc70e900ea
Jacob Boland <jacob.boland@gmail.com>
2014-10-14 19:02:34 -0700

    Use int instead of boolean for function keys class

--- manifests/global/enable_standard_function_keys.pp
+++ manifests/global/enable_standard_function_keys.pp
@@ -1,10 +1,10 @@
 # Public: Sets function keys to be standard function keys (instead of always sending Fn). Requires re-login.
-class osx::global::enable_standard_function_keys($enabled = true)  {
+class osx::global::enable_standard_function_keys($enabled = 1)  {
   boxen::osx_defaults { 'Keyboard, Use all F1, F2, etc. keys as standard function keys':
     ensure => present,
     domain => 'NSGlobalDomain',
     key    => 'com.apple.keyboard.fnState',
-    type   => 'boolean',
+    type   => 'int',
     value  => $enabled,
     user   => $::boxen_user;
   }
