cc5b6a7ae0f9f973fe8e66dba14b8ec14c1c0f6e
Jacob Boland <jacob.boland@gmail.com>
2014-10-14 19:48:10 -0700

    Use ternary instead of int

--- manifests/global/enable_standard_function_keys.pp
+++ manifests/global/enable_standard_function_keys.pp
@@ -1,11 +1,11 @@
 # Public: Sets function keys to be standard function keys (instead of always sending Fn). Requires re-login.
-class osx::global::enable_standard_function_keys($enabled = 1)  {
+class osx::global::enable_standard_function_keys($enabled = true)  {
   boxen::osx_defaults { 'Keyboard, Use all F1, F2, etc. keys as standard function keys':
     ensure => present,
     domain => 'NSGlobalDomain',
     key    => 'com.apple.keyboard.fnState',
     type   => 'int',
-    value  => $enabled,
+    value  => $enabled ? 1 : 0,
     user   => $::boxen_user;
   }
 }
